ec2b843bfe6e4517ffb508dd2806e8f5f2d829b7
Andrey <fail4iks@gmail.com>
2022-01-20 07:08:36 +0300

    Task 12

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -10,3 +10,6 @@ console.log(FCC, fact); // Change this line
 
 // 11 Add Two Numbers with JavaScript
 const sum = 10 + 10;
+
+// 12 Subtract One Number from Another with JavaScript
+const difference = 45 - 33;
