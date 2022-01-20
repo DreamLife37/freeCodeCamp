d57478a3bb9565b4d6aae64655b571d3ea104075
Andrey <fail4iks@gmail.com>
2022-01-20 07:02:39 +0300

    Task 10

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -1,3 +1,9 @@
 // 9 Explore Differences Between the var and let Keywords
 let catName = "Oliver";
 let catSound = "Meow!";
+
+// 10 Declare a Read-Only Variable with the const Keyword
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
