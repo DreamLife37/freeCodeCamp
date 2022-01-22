8291324345b92f352c2de287174a8ed1dce6e84e
Andrey <fail4iks@gmail.com>
2022-01-22 20:07:11 +0300

    Task 25

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -81,4 +81,8 @@ a /= 12; //a = a / 12;
 b /= 4;  //b = b / 4;
 c /= 11; //c = c / 11;
 
+// 25 Escaping Literal Quotes in Strings
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+//I am a "double quoted" string inside "double quotes".
+console.log(myStr)
 
