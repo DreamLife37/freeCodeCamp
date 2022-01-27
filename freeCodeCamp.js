ae386ddc7d3a6867b0f695f759894c0ee4d87bb9
Andrey <fail4iks@gmail.com>
2022-01-27 07:35:58 +0300

    Tasks 50-55

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -279,3 +279,82 @@ function reusableFunction() {
 }
 
 reusableFunction();
+
+// 50 Passing Values to Functions with Arguments
+function functionWithArgs(numberOne, numberTwo) {
+	console.log(numberOne + numberTwo)
+}
+functionWithArgs(7, 9) //16
+functionWithArgs(1, 2) //3
+
+// 51 Return a Value from a Function with Return
+function timesFive(num) {
+	return num * 5;
+}
+console.log(timesFive(2))
+console.log(timesFive(5))
+
+// 52 Global Scope and Functions
+// Declare the myGlobal variable below this line
+let myGlobal = 10
+
+function fun1() {
+	// Assign 5 to oopsGlobal Here
+	oopsGlobal = 5
+}
+
+// Only change code above this line
+function fun2() {
+	var output = "";
+	if (typeof myGlobal != "undefined") {
+		output += "myGlobal: " + myGlobal;
+	}
+	if (typeof oopsGlobal != "undefined") {
+		output += " oopsGlobal: " + oopsGlobal;
+	}
+	console.log(output);
+}
+
+// 53 Local Scope and Functions
+function myLocalScope() {
+	// Only change code below this line
+	let myVar
+	console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
+
+// 54 Global vs. Local Scope in Functions
+//Можно иметь как локальные , так и глобальные переменные с одинаковыми именами. При этом локальная переменная имеет приоритет над глобальной.
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+	// Only change code below this line
+	const outerWear = 'sweater'
+	// Only change code above this line
+	return outerWear;
+}
+
+console.log(myOutfit());
+
+// 55 Understanding Undefined Value returned from a Function
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+  sum = sum + 5
+}
+
+// Only change code above this line
+console.log(addThree());
+console.log(addFive());
+console.log(sum)
