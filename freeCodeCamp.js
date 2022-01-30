72fedb6ccfdc2927fa833259e71681cb464939fe
Andrey <fail4iks@gmail.com>
2022-01-30 23:43:51 +0300

    Tasks 61-69

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -417,3 +417,113 @@ function testEqual(val) {
 
 console.log(testEqual(10));
 console.log(testEqual("12"))
+
+// 61 Comparison with the Strict Equality Operator
+// Setup
+function testStrict(val) {
+	if (val === 7) { // Change this line
+		return "Equal";
+	}
+	return "Not Equal";
+}
+testStrict(10);
+
+// 62 Practice comparing different values
+// Setup
+function compareEquality(a, b) {
+	if (a === b) { // Change this line
+		return "Equal";
+	}
+	return "Not Equal";
+}
+
+
+// 63 Comparison with the Inequality Operator
+// Setup
+function testNotEqual(val) {
+	if (val != 99) { // Change this line
+		return "Not Equal";
+	}
+	return "Equal";
+}
+
+
+// 64 Comparison with the Strict Inequality Operator
+// Setup
+function testStrictNotEqual(val) {
+	if (val !== 17) { // Change this line
+		return "Not Equal";
+	}
+	return "Equal";
+}
+
+
+// 65 Comparison with the Greater Than Operator
+function testGreaterThan(val) {
+	if (val > 100) { // Change this line
+		return "Over 100";
+	}
+	if (val > 10) { // Change this line
+		return "Over 10";
+	}
+	return "10 or Under";
+}
+
+
+// 66 Comparison with the Greater Than Or Equal To Operator
+function testGreaterOrEqual(val) {
+	if (val >= 20) { // Change this line
+		return "20 or Over";
+	}
+	if (val >= 10) { // Change this line
+		return "10 or Over";
+	}
+	return "Less than 10";
+}
+
+// 67 Comparison with the Less Than Operator
+function testLessThan(val) {
+	if (val < 25) { // Change this line
+		return "Under 25";
+	}
+	if (val < 55) { // Change this line
+		return "Under 55";
+	}
+	return "55 or Over";
+}
+
+// 68 Comparison with the Less Than Or Equal To Operator
+function testLessOrEqual(val) {
+	if (val <= 12) { // Change this line
+		return "Smaller Than or Equal to 12";
+	}
+	if (val <= 24) { // Change this line
+		return "Smaller Than or Equal to 24";
+	}
+	return "More Than 24";
+}
+
+
+// 69 Comparisons with the Logical And Operator
+/* было:
+	if (num > 5) {
+		if (num < 10) {
+			return "Yes";
+		}
+	}
+return "No";
+
+стало:
+	if (num > 5 && num < 10) {
+		return "Yes";
+	}
+return "No"; */
+
+function testLogicalAnd(val) {
+	// Only change code below this line
+	if (val <= 50 && val >= 25) {
+		return "Yes";
+	}
+	// Only change code above this line
+	return "No";
+}
